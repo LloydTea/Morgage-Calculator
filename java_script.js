d7b45e6e0bbf95d357086d68a26e6551e6b4a08b
LloydTea <Lloyd.mft@gmail.com>
2022-07-25 18:22:04 +0400

    First Commit

---
+++ java_script.js
@@ -0,0 +1,11 @@
+var moreoption = document.getElementById("moreoptions");
+var openoptions = document.getElementById("openoptions");
+function loanoption(){
+  if (moreoption.style.display =="block") {
+    moreoption.style.display = "none";
+  }
+  else{
+    moreoption.style.display ="block";
+  }
+}
+
